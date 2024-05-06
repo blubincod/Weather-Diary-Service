cb8c167e2d5078b1605293f3f505f9ad68d867e4
JYP <rocket.jyp@gmail.com>
2024-05-06 12:54:29 +0900

    weather_diary_service_v2.0

---
+++ src/test/java/zero/weather/OpenWeatherApiTest.java
@@ -0,0 +1,12 @@
+package zero.weather;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.BDDMockito.given;
+
+public class OpenWeatherApiTest {
+    @Test
+    public void mo(){
+
+    }
+}
